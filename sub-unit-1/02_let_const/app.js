dc621bded30c6202cccff5f419573b4a70b8f8fa
Brenden Riley <BRiley001@west-mec.org>
2019-08-19 14:15:28 -0700

    completed const, let
    completed const, let

---
+++ sub-unit-1/02_let_const/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var a = 'Test1';
+let b = 'Test2';
+
+const testVar = () => {
+    const a = 30;
+    if(true) {
+        const a = 50;
+        console.log(a);
+        const a = 30;
+    }
+    console.log(`other A: ${a}`)
+}
+
+console.log(`Logged: ${a}`)
