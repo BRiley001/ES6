fb21b379c9d485cbb102c3774bb4fa3018a605ec
Brenden Riley <BRiley001@west-mec.org>
2019-08-27 15:20:51 -0700

    destructuring
    Used various array destructuring methods to get different data from an array.

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,16 +1,66 @@
-"use strict";
+let LOG = console.log;
 
-var a = 'Test1';
-let b = 'Test2';
 
-const testVar = () => {
-    const a = 30;
-    if(true) {
-        const a = 50;
-        console.log(a);
-        const a = 30;
+
+// const pizzaArray = [
+//     {
+//     name: "pep",
+//     number: 1,
+//     price: 10
+// },
+// {
+//     name: "mushroom",
+//     number: 3,
+//     price: 5
+// },
+// {
+//     name: "mango",
+//     number: 6,
+//     price: 15
+// },
+// {
+//     name: "pineapple",
+//     number: 8,
+//     price: 1000
+// }
+//     // "pep", "mushroom", "mango", "pineapple"
+// ]
+
+// // console.log(pizza[0].name);
+// const[pep, mushroom, mango, pineapple] = pizza;
+
+// console.log(pizza[0], pizza[1], pizza[2], pizza[3]);
+
+var pizzaObj = {
+    pepp: "monday",
+    array: [
+        1,
+        2,
+        3
+    ],
+    funFunction: function (name) {
+        LOG(`I'm ${name}'s function`);
     }
-    console.log(`other A: ${a}`)
+};
+
+console.log
+
+// LOG(pizzaObj["array"]);
+// pizzaObj.funFunction();
+
+const {
+    peppStr,
+    arrayArr,
+    funFunction
+} = pizzaObj;
+
+funFunction("Bobby");
+
+var dogType = "pug";
+var dog = {
+    [dogType + 'Bark']() {
+        LOG("I'm a pug!")
+    }
 }
 
-console.log(`Logged: ${a}`)
+dog.pugBark();
